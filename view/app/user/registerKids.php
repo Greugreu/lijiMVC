1d26249276cca230fc6f0d150268103924a63a22
Greugreu <fenris.fitz@hotmail.fr>
2020-04-09 15:33:25 +0200

    hotfix
    registerKids code reindentation

--- view/app/user/registerKids.php
+++ view/app/user/registerKids.php
@@ -81,7 +81,8 @@
                            value="1">
                 </div>
                 <div class="form-group">
-                    <label class="form-label" for="allergiesPlus">Renseigner son ou ses allergies, séparer par une virgule</label>
+                    <label class="form-label" for="allergiesPlus">Renseigner son ou ses allergies, séparer par une
+                                                                  virgule</label>
                     <input type="text"
                            class="form-control"
                            id="allergiesPlus"
@@ -92,10 +93,11 @@
                 </div>
                 <div class="pathologie">
                     <label for="pathologie">L'enfant as-t'il une ou des pathologies handicapantes ? :</label>
-                    <input type="checkbox" id="pathologie" name="pathologie"  value="1">
+                    <input type="checkbox" id="pathologie" name="pathologie" value="1">
                 </div>
                 <div class="form-group">
-                    <label class="form-label" for="pathologiePlus">Renseigner sa ou ses pathologies, séparer par une virgule</label>
+                    <label class="form-label" for="pathologiePlus">Renseigner sa ou ses pathologies, séparer par une
+                                                                   virgule</label>
                     <input type="text"
                            class="form-control"
                            id="pathologiePlus"
@@ -105,8 +107,16 @@
                            required>
                 </div>
                 <div class="form-group">
-                    <label class="form-label" for="message">Avez vous des consignes particulières de garderie à donner pour l'enfant ?</label>
-                    <textarea rows="5" cols="50" name="message" class="form-control" id="message" placeholder="Avez vous des consignes particulières de garderie à donner pour l'enfant ?" tabindex="4" required></textarea>
+                    <label class="form-label" for="message">Avez vous des consignes particulières de garderie à donner
+                                                            pour l'enfant ?</label>
+                    <textarea rows="5"
+                              cols="50"
+                              name="message"
+                              class="form-control"
+                              id="message"
+                              placeholder="Avez vous des consignes particulières de garderie à donner pour l'enfant ?"
+                              tabindex="4"
+                              required></textarea>
                 </div>
                 <div class="text-center">
                     <button type="submit" name="submitted" class="btn btn-start-order">Envoyer</button>
@@ -115,7 +125,6 @@
         </div>
     </div>
 </div>
-
 
 
 <?php
